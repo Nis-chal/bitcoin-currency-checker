b6bb38b130d817eb7fda0a43fb16706a93127632
Nis-chal <nischalchan@gmail.com>
2022-05-05 22:40:26 +0545

    now you can see eth and ltc rate accordind to currency

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -36,25 +36,28 @@ const apiKey = 'B03B9491-A00D-482A-B825-BA9A52A2B5C8';
 
 class CoinData {
   //3. Create the Asynchronous method getCoinData() that returns a Future (the price data).
-  Future getCoinData() async {
-    //4. Create a url combining the coinAPIURL with the currencies we're interested, BTC to USD.
-    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
-    //5. Make a GET request to the URL and wait for the response.
-    http.Response response = await http.get(Uri.parse(requestURL));
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
 
-    //6. Check that the request was successful.
-    if (response.statusCode == 200) {
-      //7. Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
-      var decodedData = jsonDecode(response.body);
-      //8. Get the last price of bitcoin with the key 'last'.
-      var lastPrice = decodedData['rate'];
-      //9. Output the lastPrice from the method.
-      return lastPrice;
-    } else {
-      //10. Handle any errors that occur during the request.
-      print(response.statusCode);
-      //Optional: throw an error if our request fails.
-      throw 'Problem with the get request';
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+
+      http.Response response = await http.get(Uri.parse(requestURL));
+
+      //6. Check that the request was successful.
+      if (response.statusCode == 200) {
+        //7. Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        //10. Handle any errors that occur during the request.
+        print(response.statusCode);
+        //Optional: throw an error if our request fails.
+        throw 'Problem with the get request';
+      }
     }
+    return cryptoPrices;
   }
 }
